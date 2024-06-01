a255d85304e349cc7252971ada0b3b7f523d2cf6
BTFLV <philip@wuschelmohr.de>
2024-06-01 07:49:01 +0200

    Python MOSI Test

--- test/test.py
+++ test/test.py
@@ -11,26 +11,27 @@ async def test_project(dut):
     dut._log.info("Start")
 
     # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, units="us")
+    clock = Clock(dut.clk, 100, units="ns")
     cocotb.start_soon(clock.start())
 
     # Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
+    # dut._log.info("Reset")
+    # dut.ena.value = 1
+    # dut.ui_in.value = 0
+    # dut.uio_in.value = 0
+    # dut.rst_n.value = 0
+    # await ClockCycles(dut.clk, 10)
+    # dut.rst_n.value = 1
 
     dut._log.info("Test project behavior")
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    for i in range(200):
+        await ClockCycles(dut.clk, 1)
+        dut._log.info("MOSI: " + str(dut.uo_out[0].value) + "SPI CLK: " + str(dut.uo_out[1].value) + "\n")
+        
 
     # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    # await ClockCycles(dut.clk, 1)
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
