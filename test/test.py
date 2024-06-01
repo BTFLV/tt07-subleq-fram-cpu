0209644047e22629948b2564080bdf92c985307b
BTFLV <philip@wuschelmohr.de>
2024-06-01 08:13:11 +0200

    Update test.py

--- test/test.py
+++ test/test.py
@@ -15,13 +15,13 @@ async def test_project(dut):
     cocotb.start_soon(clock.start())
 
     # Reset
-    # dut._log.info("Reset")
-    # dut.ena.value = 1
+    dut._log.info("Reset")
+    dut.ena.value = 1
     # dut.ui_in.value = 0
     # dut.uio_in.value = 0
-    # dut.rst_n.value = 0
-    # await ClockCycles(dut.clk, 10)
-    # dut.rst_n.value = 1
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 10)
+    dut.rst_n.value = 1
 
     dut._log.info("Test project behavior")
 
